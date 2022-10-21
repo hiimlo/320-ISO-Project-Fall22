858805407e098ef182c564f4e2a0d3d5868335f4
jbredd <joshshen118@gmail.com>
2022-10-20 23:53:18 -0400

    GA320-31: Add get node and get range endpoints

--- backend/LMP^2/routes/api.js
+++ backend/LMP^2/routes/api.js
@@ -7,9 +7,36 @@ router.get('/data', async (req, res, next) => {
     res.send(data)
 });
 
+router.get('/data/range', async (req, res) => {
+    const nname = req.query.nname
+    const start = req.query.start
+    const end = req.query.end
+
+    const data = await Data.find({
+        pnode_name: nname,
+        period_id: {
+            $gte: start,
+            $lte: end
+        }
+    });
+    res.send(data)
+})
+//test link: http://localhost:3000/api/data/range?nname=.I.KENT%20%20%20%20345%202&start=17-JUL-2020%2001&end=17-JUL-2020%2015
+
+
+router.get('/data/node', async (req, res) => {
+    const data = await Data.find({
+        pnode_name: req.query.nname,
+        period_id: req.query.period
+    });
+    res.send(data)
+})
+// test link: http://localhost:3000/api/data/node?nname=.I.KENT%20%20%20%20345%202&period=17-JUL-2020%2001
+
 router.get('/data/:node', async (req, res) => {
     const data = await Data.find({pnode_name: req.params.node});
     res.send(data)
 })
+
 
 module.exports = router;
