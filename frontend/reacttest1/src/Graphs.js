1d27c83d8ce165c630d5c73bbb9c4691d266bba4
Ayush Chaudhary <ayushc.pro@gmail.com>
2022-10-28 03:02:22 -0400

    Added 3 hardcoded nodes (alex)

--- frontend/reacttest1/src/Graphs.js
+++ frontend/reacttest1/src/Graphs.js
@@ -1,96 +1,96 @@
 import React from 'react';
 import './App.css';
-import {Scatter} from 'react-chartjs-2';
-import {CategoryScale} from 'chart.js'; 
+import { Scatter } from 'react-chartjs-2';
+import { CategoryScale } from 'chart.js';
 import { Chart as ChartJS } from 'chart.js/auto'
-import { Chart }    from 'react-chartjs-2'
+import { Chart } from 'react-chartjs-2'
 import { Link } from "react-router-dom";
 //Chart.register(CategoryScale);
 
 
 const data = require('./localhost.json');
-let _id = data.map(function(e) {return e._id});
-let scenario_id = data.map(function(e) {return e.scenario_id});
-let pnode_name = data.map(function(e) {return e.pnode_name});
-let period_id = data.map(function(e) {return e.period_id});
-let lmp = data.map(function(e) { return e.lmp});
-
-let kent = data.filter(function(e) {
-  if (e.pnode_name == ".I.KENT    345 2") {
-    return e;
-  } 
-})
-
-let dataPoint = kent.map(function(e) {
-  return {
-    x : e.period_id.charCodeAt(e.period_id.length-1)-48, 
-    y: e.lmp
-   
-  }
-})
-const state1 = {
-  labels: pnode_name,
-  datasets: [
-    {
-      label: 'Nodes',
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: dataPoint,
-      options: {
-        scales: {
-          x: {
-            ticks: {
-              // Include a dollar sign in the ticks
-              callback: function(value, index, ticks) {
-                  return 'Day ' + value;
-              }
-            },
-            position: 'bottom'
-          },
-          y: {
-            beginAtZero: true,
-          }
-        }
-      }
-    }
-  ]
-}
 
 export default class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {node: '.I.KENT    345 2'};
+    this.changeNode = this.changeNode.bind(this);
+  }
+
+  changeNode(event, name) {
+    this.setState({
+      node: name
+    })
+  }
+
+  createGraphState() {
+    const nodeName = this.state.node;
+    let node = data.filter(function (e) {
+      if (nodeName === e.pnode_name) {
+        return e;
+      }
+    })
+    
+    let dataPoint = node.map(function (e) {
+      return {
+        x: e.period_id.charCodeAt(e.period_id.length - 1) - 48,
+        y: e.lmp
+      }
+    })
+
+    const graphState = {
+      labels: node,
+      datasets: [
+        {
+          label: 'Nodes',
+          backgroundColor: 'rgba(75,192,192,1)',
+          borderColor: 'rgba(0,0,0,1)',
+          borderWidth: 2,
+          data: dataPoint,
+        }
+      ]
+    }
+    return graphState;
+  }
+
   render() {
     return (
       <div className="App-header">
-      <Link className="App-link" to="/home">Go Home</Link>
-          <Scatter className = "Grapher"
-            data={state1}
-            options={{
-              title: {
-                display: true,
-                text: 'LMP Of Nodes',
-                fontSize: 20
-              },
-              legend: {
-                display: true,
-                position: 'right'
-              },
-              scales: {
-                x: {
-                  ticks: {
-                    // Include a dollar sign in the ticks
-                    callback: function(value, index, ticks) {
-                        return 'Day ' + value;
-                    }
-                  },
-                  position: 'bottom'
+        <Link className="App-link" to="/home">Go Home</Link>
+        <div className="Alert">NOTE: this is dummy for a proof of concept</div>
+        <button onClick = {event => this.changeNode(event, '.I.KENT    345 2')}>.I.KENT    345 2</button>
+        <button onClick = {event => this.changeNode(event, '.I.PARKCITY345 1')}>.I.PARKCITY345 1</button>
+        <button onClick = {event => this.changeNode(event, '.I.VICTORIA345 1')}>.I.VICTORIA345 1</button>
+        <div>{this.state.node}</div>
+        <Scatter className="Grapher"
+          data={this.createGraphState()}
+          options={{
+            title: {
+              display: true,
+              text: 'LMP Of Nodes',
+              fontSize: 20
+            },
+            legend: {
+              display: true,
+              position: 'right'
+            },
+            scales: {
+              x: {
+                ticks: {
+                  // Include a dollar sign in the ticks
+                  callback: function (value, index, ticks) {
+                    return 'Day ' + value;
+                  }
                 },
-                y: {
-                  beginAtZero: true,
-                }
+                position: 'bottom'
+              },
+              y: {
+                beginAtZero: true,
               }
-            }} />
-        </div>
-        
+            }
+          }} />
+      </div>
+
     );
   }
 }
