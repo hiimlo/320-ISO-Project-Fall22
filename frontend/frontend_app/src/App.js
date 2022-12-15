7d025a01e73d2f388db814b4909b56ca20bc60cb
jbredd <joshshen118@gmail.com>
2022-12-15 16:16:37 -0500

    Add Prettier config
    vscode extension enforcing 4 whitespace tabbing. the old tabbing was driving me crazy

--- frontend/frontend_app/src/App.js
+++ frontend/frontend_app/src/App.js
@@ -1,49 +1,53 @@
 //import './App.css';
-import { BrowserRouter as Router,
-   Switch, 
-   Route,
-   Routes,
-   Navigate,} from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Routes,
+    Navigate
+} from 'react-router-dom';
 
-import Home from "./components/Home"
-import Graph from "./components/Graphs"
-import ShowData from "./components/ShowData";
-import Stats from "./components/Stats"
+import Home from './components/screens/Home';
+import Graph from './components/screens/Graphs';
+import UC1 from './components/screens/UC1';
+import ShowData from './components/ShowData';
+import Stats from './components/screens/Stats';
 
 function App() {
-  return (
-    <>
-    {/* This is the alias of BrowserRouter i.e. Router */}
-    
-    <Router>
-      <Routes>
-      {/* This route is for home component 
+    return (
+        <>
+            {/* This is the alias of BrowserRouter i.e. Router */}
+
+            <Router>
+                <Routes>
+                    {/* This route is for home component 
       with exact path "/", in component props 
       we passes the imported component*/}
-      <Route path="/home" element={<Home />} />
-      
-        
-      {/* This route is for about component 
-      with exact path "/about", in component 
-      props we passes the imported component*/}
-      <Route path="/graph" element={<Graph />} />
+                    <Route path="/home" element={<Home />} />
 
-      {/* This route is for about component 
+                    {/* This route is for about component 
       with exact path "/about", in component 
       props we passes the imported component*/}
-      <Route path="/stats" element={<Stats />} />
-     
-    
-        
-      {/* If any route mismatches the upper 
+                    <Route path="/graph" element={<Graph />} />
+
+                    {/* This route is for about component 
+      with exact path "/about", in component 
+      props we passes the imported component*/}
+                    <Route path="/uc1" element={<UC1 />} />
+
+                    {/* This route is for about component 
+      with exact path "/about", in component 
+      props we passes the imported component*/}
+                    <Route path="/stats" element={<Stats />} />
+
+                    {/* If any route mismatches the upper 
       route endpoints then, redirect triggers 
       and redirects app to home component with to="/" */}
-      <Route path="/" element={<Navigate replace to="/home" />} />
-      </Routes>
-
-    </Router>
-    </>
-  );
+                    <Route path="/" element={<Navigate replace to="/home" />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
