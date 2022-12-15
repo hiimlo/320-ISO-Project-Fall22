330d04d622b4f7bf686d733a32aaf4434e78e2f3
jbredd <joshshen118@gmail.com>
2022-12-15 16:18:12 -0500

    Move screen components to own folder
    Home, Graphs, and Stats used to be in components/, now there are in components/screens/. purely for organizational purposes and ease of mind

---
+++ frontend/frontend_app/src/components/screens/Home.js
@@ -0,0 +1,29 @@
+import isologo from '../../images/ISO Logo clear.png';
+//import logo from './images/logo.svg';
+import '../../App.css';
+import { Link } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={isologo} className="iso-App-logo" alt="logo" />
+
+                <p className="title">
+                    Hello Team. Here's the landing page for our project.
+                </p>
+                <br></br>
+                <div className="stackRow">
+                    <Link className="App-link" to="/uc1">
+                        Graph
+                    </Link>
+                    <Link className="App-link" to="/stats">
+                        Stats
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
