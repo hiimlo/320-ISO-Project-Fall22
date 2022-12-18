f424f877e29ec66228b109af4e5d92e9ac433bf6
jbredd <joshshen118@gmail.com>
2022-12-17 22:04:03 -0500

    Make static material-table

--- frontend/frontend_app/src/components/charts/StatsTable.js
+++ frontend/frontend_app/src/components/charts/StatsTable.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
+import MaterialTable from 'material-table'
+import tableIcons from './MaterialTableIcons'
 
 export default class StatsTable extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            data: this.makeData(props.data)
+            data: this.makeData(props.data),
+            rows: [
+                { name: 'Mohammad', surname: 'Faisal', birthYear: 1995 },
+                { name: 'Nayeem Raihan ', surname: 'Shuvo', birthYear: 1994 }
+            ],
+            columns: [
+                { title: 'Name', field: 'name' },
+                { title: 'Surname', field: 'surname' },
+                { title: 'Birth Year', field: 'birthYear', type: 'numeric' }
+            ]
         }
     }
 
@@ -21,32 +25,15 @@ export default class StatsTable extends Component {
 
     render() {
         return (
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {this.state.data.map((row) => (
-                            <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                                <TableCell align="right">{row.carbs}</TableCell>
-                                <TableCell align="right">{row.protein}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            <div style={{ maxWidth: 1000 }}>
+                <MaterialTable
+                    columns={this.state.columns}
+                    data={this.state.rows}
+                    title="Stats Table"
+                    icons={tableIcons}
+                    exportButton={true}
+                />
+            </div>
         )
     }
 }
